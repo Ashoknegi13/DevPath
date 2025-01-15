ee2049582ecd7dc6297ef4ac4e32bd431af0ecfa
Ashok <negiashok132@gmail.com>
2025-01-15 21:35:04 +0530

    ..

--- signin.php
+++ signin.php
@@ -1,9 +1,13 @@
 <?php
 include 'nav.php'; 
-
 ?>
-<html>
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Login </title>
+ 
 <head>
 	<title>Dev Path login Page</title>
 	<style>
